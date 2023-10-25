f8a77b143aa4bc2c5947b86f7f8c26ffb226addc
Hanttogang <saehyun9906@naver.com>
2023-10-25 11:27:01 +0900

    SwirftUIMemoApp Step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by Teameverywhere on 10/25/23.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
