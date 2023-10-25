6dcf1d7a2fb6a58bc95ef38a0649fde421b39c46
Hanttogang <saehyun9906@naver.com>
2023-10-25 14:31:06 +0900

    메모장 만들기: 메모 자세히 보기 화면

---
+++ SwiftUIMemo/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  SwiftUIMemo
+//
+//  Created by Teameverywhere on 10/25/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
